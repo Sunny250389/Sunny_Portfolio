85bc1a8d1a1e2473ddf69e2509bcb2251fa29464
Sunny250389 <32661353+Sunny250389@users.noreply.github.com>
2025-06-26 13:39:25 +0530

    Update main.tsx
    changes it to java script file

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,20 @@
+// import { StrictMode } from 'react';
+// import { createRoot } from 'react-dom/client';
+// import App from './App.tsx';
+// import './index.css';
+
+// createRoot(document.getElementById('root')!).render(
+//   <StrictMode>
+//     <App />
+//   </StrictMode>
+// );
+
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
-import App from './App.tsx';
+import App from './App';
 import './index.css';
 
-createRoot(document.getElementById('root')!).render(
+createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
   </StrictMode>
