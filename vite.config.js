1288625744b274cc8df9bb2a3aa25918d1a1ba57
Sunny250389 <32661353+Sunny250389@users.noreply.github.com>
2025-06-29 10:37:21 +0530

    Update vite.config.js
    updated vite config js

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,18 @@
+// import { defineConfig } from 'vite';
+// import react from '@vitejs/plugin-react';
+
+// // https://vitejs.dev/config/
+// export default defineConfig({
+//   plugins: [react()],
+//   optimizeDeps: {
+//     exclude: ['lucide-react'],
+//   },
+// });
+
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
+  base: '/<repo-name>/', // <- required for GitHub Pages
   plugins: [react()],
-  optimizeDeps: {
-    exclude: ['lucide-react'],
-  },
 });
