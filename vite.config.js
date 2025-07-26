1a590fb632b59f1bfec39830288d365fadfce261
Sunny Kumar <sunny250389@gmail.com>
2025-07-27 02:52:46 +0530

    Adding custom domain

--- vite.config.js
+++ vite.config.js
@@ -10,10 +10,21 @@
 // });
 
 // vite.config.js
+//import { defineConfig } from 'vite';
+//import react from '@vitejs/plugin-react';
+//
+//export default defineConfig({
+//  base: '/Sunny_Portfolio/',  // <-- Add this line
+//  plugins: [react()],
+//});
+
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  base: '/Sunny_Portfolio/',  // <-- Add this line
   plugins: [react()],
+  base: '/', // 👈 root base, because you're on a custom domain
+  optimizeDeps: {
+    exclude: ['lucide-react'],
+  },
 });
